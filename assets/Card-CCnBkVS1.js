44e7613efa2a5133c8a0b57462806cb99f7c383f
afx786 <afx786@users.noreply.github.com>
2026-07-22 20:09:58 +0000

    deploy: cde87f7177c344eaa44ff3f6b180d2ea2eb64f56

---
+++ assets/Card-CCnBkVS1.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-Q_scY88D.js";function c({children:r,className:o="",hover:a=!1}){return e.jsx("section",{className:`rounded-xl p-lg card-border ${a?"card-hover":""} ${o}`,style:{background:"rgb(var(--color-surface-container-low))"},children:r})}export{c as C};
