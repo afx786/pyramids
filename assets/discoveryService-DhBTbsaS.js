c1e4db5781a3c96d54b13b0c97f1e7d49ee66640
afx786 <afx786@users.noreply.github.com>
2026-07-23 18:34:08 +0000

    deploy: e8651a418ae175f396ace9b243d52f9db6c12326

--- assets/discoveryService-DhBTbsaS.js
+++
@@ -1,1 +0,0 @@
-import{a as e}from"./index-D_V5-8_8.js";const r={listTeams(){return e.get("/teams")},listHackathons(){return e.get("/hackathons")},getFeed(t="all"){return e.get(`/feed?type=${t}`)}};export{r as d};
