bc41bebda4b722ce01bf4a7450d16fad3d4658c1
afx786 <afx786@users.noreply.github.com>
2026-07-23 19:57:01 +0000

    deploy: 3c07f8add94fe994b6faba75dce19bea9e10be22

--- assets/discoveryService-BPMctqH3.js
+++
@@ -1,1 +0,0 @@
-import{a as e}from"./index-CYTQrqDD.js";const r={listTeams(){return e.get("/teams")},listHackathons(){return e.get("/hackathons")},getFeed(t="all"){return e.get(`/feed?type=${t}`)}};export{r as d};
